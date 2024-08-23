b7ad789c20f1a38b5624eb4c36c7bbd4035788ec
Ciaron Rider <ciaron@ridernator.com>
2024-08-23 17:56:17 +0100

    Added solution to day 2 part 1 in python

---
+++ 02/python/part1.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+inputFile = open("../data/input", "r")
+
+totalArea = 0
+
+for line in inputFile.readlines():
+    dimensions = line.split("x")
+    length = int(dimensions[0])
+    width = int(dimensions[1])
+    height = int(dimensions[2])
+
+    area = 2 * (width * height + width * length + height * length)
+    slack = min(width * height, width * length, height * length)
+
+    totalArea += area + slack
+
+print("Total area is", totalArea, "square feet")
